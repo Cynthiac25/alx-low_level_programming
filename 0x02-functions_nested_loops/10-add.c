e900a6fc6334c2a6f72b3a3865600a6a80e7b79f
Cynthiac25 <cynthiamokgoebo@gmail.com>
2023-03-21 15:14:55 -0700

    commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - a function that adds two values
+ *
+ * @n1: first number input
+ * @n2: second munber input
+ * Return: integer value
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
