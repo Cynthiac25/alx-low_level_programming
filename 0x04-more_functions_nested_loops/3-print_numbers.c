f5f6eba22befc217f9011bbe2cb1664b9608a193
Cynthiac25 <cynthiamokgoebo@gmail.com>
2023-03-23 09:32:33 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - a function that prints the numbers
+ *
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int m;
+
+	for (m = 48; m < 58; m++)
+	{
+		_putchar(m);
+	}
+		_putchar('\n');
+}
